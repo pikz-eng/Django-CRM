716c65e2af6fa5675e009621b892e05317ff1b4a
marius <mariusdsand@gmail.com>
2024-11-27 16:35:41 +0200

    registration

---
+++ mysite/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+def home(request):
+    # check to see if loggin in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, 'Esti logat!')
+            return redirect('home')
+        else:
+            messages.success(request, 'User-ul nu existaTry again!')
+            return redirect('home')
+    else:
+        return render(request, 'home.html',  {})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out!')
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
+
